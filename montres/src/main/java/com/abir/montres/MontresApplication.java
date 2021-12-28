c671f14f4316d1ae097a18af6f39da8cc3aef57c
SampleUser <sample@example.com>
2021-12-28 14:40:00 +0000

    first commit

---
+++ montres/src/main/java/com/abir/montres/MontresApplication.java
@@ -0,0 +1,13 @@
+package com.abir.montres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MontresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MontresApplication.class, args);
+	}
+
+}
